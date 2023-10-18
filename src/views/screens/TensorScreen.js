08fce0508a4e29b07fb6b163b0f954c09e5bd274
HaroldSIMO <harold.simo-tenekam.1@ens.etsmtl.ca>
2023-10-18 03:10:44 -0400

    update

--- src/views/screens/TensorScreen.js
+++ src/views/screens/TensorScreen.js
@@ -21,6 +21,7 @@ import { useSelector } from "react-redux";
 import { addDoc, collection } from "firebase/firestore";
 import { db, storage } from "../../firebase.config";
 import { ref, uploadBytesResumable } from "firebase/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 
@@ -43,49 +44,9 @@ export default function TensorScreen({ navigation }) {
       console.log("[+] Loading pre-trained images detection model");
       //Replce model.json and group1-shard.bin with your own custom model
       const modelJson = await require("../../../assets/model/model.json");
-      const modelWeights1 = await require("../../../assets/model/group1-shard1of41.bin");
-      const modelWeights2 = await require("../../../assets/model/group1-shard2of41.bin");
-      const modelWeights3 = await require("../../../assets/model/group1-shard3of41.bin");
-      const modelWeights4 = await require("../../../assets/model/group1-shard4of41.bin");
-      const modelWeights5 = await require("../../../assets/model/group1-shard5of41.bin");
-      const modelWeights6 = await require("../../../assets/model/group1-shard6of41.bin");
-      const modelWeights7 = await require("../../../assets/model/group1-shard7of41.bin");
-      const modelWeights8 = await require("../../../assets/model/group1-shard8of41.bin");
-      const modelWeights9 = await require("../../../assets/model/group1-shard9of41.bin");
-      const modelWeights10 = await require("../../../assets/model/group1-shard10of41.bin");
-      const modelWeights11 = await require("../../../assets/model/group1-shard11of41.bin");
-      const modelWeights12 = await require("../../../assets/model/group1-shard12of41.bin");
-      const modelWeights13 = await require("../../../assets/model/group1-shard13of41.bin");
-      const modelWeights14 = await require("../../../assets/model/group1-shard14of41.bin");
-      const modelWeights15 = await require("../../../assets/model/group1-shard15of41.bin");
-      const modelWeights16 = await require("../../../assets/model/group1-shard16of41.bin");
-      const modelWeights17 = await require("../../../assets/model/group1-shard17of41.bin");
-      const modelWeights18 = await require("../../../assets/model/group1-shard18of41.bin");
-      const modelWeights19 = await require("../../../assets/model/group1-shard19of41.bin");
-      const modelWeights20 = await require("../../../assets/model/group1-shard20of41.bin");
-      const modelWeights21 = await require("../../../assets/model/group1-shard21of41.bin");
-      const modelWeights22 = await require("../../../assets/model/group1-shard22of41.bin");
-      const modelWeights23 = await require("../../../assets/model/group1-shard23of41.bin");
-      const modelWeights24 = await require("../../../assets/model/group1-shard24of41.bin");
-      const modelWeights25 = await require("../../../assets/model/group1-shard25of41.bin");
-      const modelWeights26 = await require("../../../assets/model/group1-shard26of41.bin");
-      const modelWeights27 = await require("../../../assets/model/group1-shard27of41.bin");
-      const modelWeights28 = await require("../../../assets/model/group1-shard28of41.bin");
-      const modelWeights29 = await require("../../../assets/model/group1-shard29of41.bin");
-      const modelWeights30 = await require("../../../assets/model/group1-shard30of41.bin");
-      const modelWeights31 = await require("../../../assets/model/group1-shard31of41.bin");
-      const modelWeights32 = await require("../../../assets/model/group1-shard32of41.bin");
-      const modelWeights33 = await require("../../../assets/model/group1-shard33of41.bin");
-      const modelWeights34 = await require("../../../assets/model/group1-shard34of41.bin");
-      const modelWeights35 = await require("../../../assets/model/group1-shard35of41.bin");
-      const modelWeights36 = await require("../../../assets/model/group1-shard36of41.bin");
-      const modelWeights37 = await require("../../../assets/model/group1-shard37of41.bin");
-      const modelWeights38 = await require("../../../assets/model/group1-shard38of41.bin");
-      const modelWeights39 = await require("../../../assets/model/group1-shard39of41.bin");
-      const modelWeights40 = await require("../../../assets/model/group1-shard40of41.bin");
-      const modelWeights41 = await require("../../../assets/model/group1-shard41of41.bin");
+      const modelWeights1 = await require("../../../assets/model/group1-shard1of1.bin");
 
-      const ioHandler = bundleResourceIO(modelJson, [modelWeights1, modelWeights2, modelWeights3, modelWeights4, modelWeights5,modelWeights6,modelWeights7,modelWeights8,modelWeights9,modelWeights10,modelWeights11,modelWeights12,modelWeights13,modelWeights14,modelWeights15,modelWeights16,modelWeights17,modelWeights18,modelWeights19,modelWeights20,modelWeights21,modelWeights22,modelWeights23,modelWeights24,modelWeights25,modelWeights26,modelWeights27,modelWeights28,modelWeights29,modelWeights30,modelWeights31,modelWeights32,modelWeights33,modelWeights34,modelWeights35,modelWeights36,modelWeights37,modelWeights38,modelWeights39,modelWeights40,modelWeights41]);
+      const ioHandler = bundleResourceIO(modelJson, [modelWeights1]);
       const GradingNetwork1 = await tf.loadGraphModel(
         ioHandler
       );
@@ -126,7 +87,7 @@ export default function TensorScreen({ navigation }) {
       const imgBufferTop = tf.util.encodeString(img64Top, "base64").buffer;
       const rawImageDataTop = new Uint8Array(imgBufferTop);
 
-      let imageTensorTop = imageToTensor(rawImageDataTop).resizeBilinear([299,299]);
+      let imageTensorTop = imageToTensor(rawImageDataTop).resizeBilinear([224,224]);
 
 
       console.log("[+] Retrieving image 2 from link :" + images[1]?.uri);
@@ -136,28 +97,42 @@ export default function TensorScreen({ navigation }) {
       const imgBufferDown = tf.util.encodeString(img64Down, "base64").buffer;
       const rawImageDataDown = new Uint8Array(imgBufferDown);
 
-      let imageTensorDown = imageToTensor(rawImageDataDown).resizeBilinear([299,299]);
+      let imageTensorDown = imageToTensor(rawImageDataDown).resizeBilinear([224,224]);
       
 
-      imageTensorTop = imageTensorTop.resizeBilinear([299,299]).reshape([1,299,299,3])
-      imageTensorDown = imageTensorDown.resizeBilinear([299,299]).reshape([1,299,299,3])
+      imageTensorTop = imageTensorTop.resizeBilinear([224,224]).reshape([1,224,224,3])
+      imageTensorDown = imageTensorDown.resizeBilinear([224,224]).reshape([1,224,224,3])
+ 
 
-      let result = await GradingNetwork.predict([imageTensorTop,imageTensorDown]).dataSync()
-      console.log(result)
-      let granulometrie = [0,0,...result];
-      let accumuledResult = [0,0,0,0,0,0,0,0]
-      for(i=0;i<7;i++){
-        if(i==0){
-          accumuledResult[6-i] = 1-granulometrie[6-i];
-        }else{
-          accumuledResult[6-i] = Math.max(accumuledResult[7-i]-granulometrie[6-i],0)
-        }
-          }
-          accumuledResult[7] = granulometrie[7]
-          console.log(accumuledResult);
-      setResult(accumuledResult);
+      let testPictures = [imageTensorTop,imageTensorDown]
 
-      console.log("_______Successfully predicted particle size curve");
+      let granularPictures = true;
+      for(i=0;i<=1;i++){
+        let granularMaterialPredict = await GradingNetwork.predict(testPictures[i]).dataSync();
+        console.log(granularMaterialPredict);
+        if(granularMaterialPredict >= 0.5 ){
+         granularPictures = false;
+      }}
+      
+      if(granularPictures == true){
+        //let result = await GradingNetwork.predict([imageTensorTop,imageTensorDown]).dataSync()
+        console.log('materiau granulaire')
+       // let granulometrie = [0,0,...result];
+      //  let accumuledResult = [0,0,0,0,0,0,0,0]
+      //  for(i=0;i<7;i++){
+       //   if(i==0){
+       //     accumuledResult[6-i] = 1-granulometrie[6-i];
+       //   }else{
+       //     accumuledResult[6-i] = Math.max(accumuledResult[7-i]-granulometrie[6-i],0)
+        //  }
+        //    }
+        //    accumuledResult[7] = granulometrie[7]
+        //    console.log(accumuledResult);
+       // setResult(accumuledResult);
+        console.log("_______Successfully predicted particle size curve");
+      }else{
+        console.log("insérer des images valides");
+      }
 
 
       let newAnalysis_infos = await AsyncStorage.getItem("newAnalysis_infos")
